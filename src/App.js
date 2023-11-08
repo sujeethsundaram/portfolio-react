58c21d839a6d6e7cf873d35218819677c44f4cec
Sujeeth <sujeeth1807@gmail.com>
2023-11-08 15:14:39 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <h1>Sujeeth Sundaram</h1>
+        <p>Web Developement, Data Science, Machine Learning
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>🡇</h1>
       </header>
+      <section className="About">
+        <h2>About Me</h2>
+        <p>
+          I am a 3rd year student at VIT Chennai pursuing B.Tech Computer Science with Specialization in AI and Machine Learning.
+        </p>
+      </section>
+      <section className="Projects">
+        <h2>Projects</h2>
+        <ul>
+          <li>
+            <strong>Project 1:</strong> Made an ML model thats detects handwritten digits and identifies them.
+          </li>
+          <li>
+            <strong>Project 2:</strong> and ML model that detects a picture of a Pokemon and identifies its name.
+          </li>
+          <li>
+            <strong>Project 3:</strong> A self learning AI snake game using deep reinforcement learning. 
+          </li>
+        </ul>
+      </section>
+      <section className="Contact">
+        <h2>Contact</h2>
+        <p>Email: sujeeth1807@gmail.com</p>
+        <p>GitHub: <a href="https://github.com/sujeethsundaram">github.com/sujeethsundaram</a></p>
+      </section>
     </div>
   );
 }
 
 export default App;
+
